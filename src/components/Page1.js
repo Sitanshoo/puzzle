8a57bfab7c2b170949766d26639c3f696c0cbc41
Sitanshu Mustafi <sitanshumustafi95035@gmail.com>
2022-11-23 00:06:51 +0530

    Initail setup

---
+++ src/components/Page1.js
@@ -0,0 +1,12 @@
+import CustomizedAccordions from "./common/CustomizedAccordions";
+
+const Page1 = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <CustomizedAccordions />
+    </>
+  );
+};
+
+export default Page1;
